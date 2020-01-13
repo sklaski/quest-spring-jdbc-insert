b0c67f527e234fd3a6e26f131bcb3f0237d57e44
Steffen Klaski <steffen.klaski@t-systems.com>
2020-01-13 13:31:17 +0100

    SchoolRepository.java sub save created

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -3,7 +3,6 @@ package com.wildcodeschool.wildandwizard.controller;
 import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,7 +18,6 @@ public class SchoolController {
                              @RequestParam String country
     ) {
         model.addAttribute("school", repository.save(name, capacity, country));
-
         return "school_get";
     }
 }
--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -3,7 +3,6 @@ package com.wildcodeschool.wildandwizard.controller;
 import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
